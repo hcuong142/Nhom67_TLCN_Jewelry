1bf16569704ed082847f0fe32165c697ab5d9dcc
0768800022 <huycuong140203@gmail.com>
2024-10-01 15:06:17 +0700

    update-header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,9 @@
+import styles from './Home.module.scss';
+
 function Home() {
-    return <h2>Home Page</h2>;
+    return <div className={styles.wrapper}>
+        <img className={styles.img} src="//bizweb.dktcdn.net/100/461/213/themes/870653/assets/slider_1.jpg?1727430576753" alt="Bộ Sưu Tập Resort ' 2024" class="img-responsive center-block"></img>
+    </div>;
 }
 
 export default Home;
